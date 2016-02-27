91ec2ea4d7c0e37915764f58bf57ab6938ccf222
Ivor Wanders <ivor@iwanders.net>
2016-02-27 13:40:47 +0100

    Also respect initialization for the non-SoftSerial example.
    Alse forced the init to be perceived as successful, now respects result of
    the handshake. Also adds the enable pin.

--- examples/reader/reader.ino
+++ examples/reader/reader.ino
@@ -3,6 +3,7 @@
 
 #define RX_PIN 0
 #define TX_PIN 1
+#define EN_PIN 2
 
 
 OBD9141 obd;
@@ -11,6 +12,9 @@ OBD9141 obd;
 void setup(){
     Serial.begin(9600);
     delay(2000);
+
+    pinMode(EN_PIN, OUTPUT);
+    digitalWrite(EN_PIN, HIGH);
 
     obd.begin(Serial1, RX_PIN, TX_PIN);
 
@@ -23,7 +27,10 @@ void loop(){
     Serial.print("init_success:");
     Serial.println(init_success);
 
-    init_success = true; // force succes when using simulator
+    //init_success = true;
+    // Uncomment this line if you use the simulator to force the init to be
+    // interpreted as successful. With an actual ECU; be sure that the init is 
+    // succesful before trying to request PID's.
 
     if (init_success){
         bool res;
